69ef4f5fb8ae62c9bb0bf7d714b1b172aff4e0de
unknown <r4jdeepbiswas@gmail.com>
2020-04-17 15:46:14 +0530

    testing

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def main():
     my_output = f"Hello {my_input}"
 
     print(f"::set-output name=myOutput::{my_output}")
-
+    with open('bamboozle.txt', 'a') as f:
+    	write("this works")
+    	
 
 if __name__ == "__main__":
     main()
